74ab71576d78a7b0c51fff003eba1e52eee2c6e3
wikk <belovedwik@gmail.com>
2016-12-10 00:05:23 +0200

    BackLinks - ok

--- EDF Modules/BackLinks/BackLinks.cs
+++ EDF Modules/BackLinks/BackLinks.cs
@@ -7,9 +7,19 @@ using System.Web;
 using HtmlAgilityPack;
 using BackLinks;
 using Databox.Libs.BackLinks;
+using System.Text.RegularExpressions;
+using System.Globalization;
 
 namespace WheelsScraper
 {
+    public static class StringExt {
+        public static string ClearQ(this string str){
+            return str.Replace("'","");
+        }
+        public static string GetStrQ(string str) {
+            return str != null ? str.ClearQ() : string.Empty;
+        }
+    }
     public class BackLinks : BaseScraper
     {
         public BackLinks()
@@ -80,16 +90,21 @@ namespace WheelsScraper
 
         protected override void RealStartProcess()
         {
-            var html = PageRetriever.ReadFromServer(Url + "advertisers/dir.php?t=block&l=http://www.ecommercespot.com", true);
+            string ecomerceLink = "t=block&l=http://www.ecommercespot.com";
+            var html = PageRetriever.ReadFromServer(Url + "advertisers/dir.php?" + ecomerceLink, true);
 
             // find categories 
             var doc = CreateDoc(html);
-            var _categories = doc.DocumentNode.SelectNodes("//div[@id='admincontentbox']/div/table//td[@class='DataTD']//a");
+            var _select = doc.DocumentNode.SelectNodes("//option[ancestor::select[@name='CID']]");
 
-            foreach (var cat in _categories) {
-                var catName = cat.InnerTextOrNull();
-                if (extSett.CategotyList.Any( n=> n.CategoryName.Contains(catName))) { 
-                    lstProcessQueue.Add(new ProcessQueueItem { URL = cat.AttributeOrNull("href"), ItemType = 1, Name = catName });
+            foreach (var option in _select)
+            {
+                var catId = option.AttributeOrNull("value");
+                var catName = option.NextSibling.InnerTextOrNull();
+                if (extSett.CategotyList.Any(n => n.CategoryName!= null && n.CategoryName.Contains(catName)))
+                {
+                    var catUrl = Url + "advertisers/links.php?" + ecomerceLink + "&CID=" + catId + "&f=all&cr=US&lng=en";
+                    lstProcessQueue.Add(new ProcessQueueItem { URL = catUrl, ItemType = 2, Name = catName, Item ="1" });
                 }
             }
 
@@ -97,21 +112,84 @@ namespace WheelsScraper
             StartOrPushPropertiesThread();
         }
 
-        protected void ProcessBrandsListPage(ProcessQueueItem pqi)
+        protected void ProcessCategoryListPage(ProcessQueueItem pqi)
         {
+            var page = (string)pqi.Item;
             if (cancel)
                 return;
 
+            MessagePrinter.PrintMessage("Start process category " + pqi.Name + ", page" + page);
+
+            var html = PageRetriever.ReadFromServer(pqi.URL, true);
+            var doc = CreateDoc(html);
+            
+            // find links on page
+            var links = doc.DocumentNode.SelectNodes("//table//td[contains(@class,'DataTD') and contains(@class ,'smallb')]");
+            if (links == null)
+            {
+                pqi.Processed = true;
+                return;
+            }
+
+            foreach (var link in links.Take(1)) {
+                
+                var wi = new ExtWareInfo();
+                wi.Title = link.SelectSingleNode(".//span[@class='smallb']").InnerTextOrNull();
+                MessagePrinter.PrintMessage("  Process " + pqi.Name + " / "+wi.Title);
+                wi.LandingPage = link.SelectSingleNode(".//span[@class='smallb']/following-sibling::text()").InnerTextOrNull().Replace(Environment.NewLine,"").Trim();
+                wi.PriceText = link.SelectSingleNode(".//font[@class='smallb']").InnerTextOrNull();
+                Regex regPrice = new Regex(@"[0-9]+(\.[0-9]+)?");
+                var price = !string.IsNullOrEmpty(wi.PriceText) ? regPrice.Match(wi.PriceText).Value : "0";
+                wi.Price = decimal.Parse(price, CultureInfo.InvariantCulture);
+
+                //            0        1        2        3          4           5        6      7
+                //showUrl(page_id, GooglePR, MozRank, AlexaRank, BackLinks, DomainAge, MOZDA, LinkUrl)
+                //showUrl(917164, '23.62', '4.9',  '10130615', '792', '12', '23.62', '')
+                var showUrl = link.SelectSingleNode(".//span[@class='smallb']").AttributeOrNull("onclick");
+                Regex regVal = new Regex(@"\(.*?\)");
+                showUrl = regVal.Match(showUrl).Value.Replace("(","").Replace(")","");
+                var arr = showUrl.Split(',');
+                if (arr.Length > 0) {
+                    wi.id = StringExt.GetStrQ(arr[0]);
+                    wi.Moz = StringExt.GetStrQ(arr[6]);
+                    wi.Backlinks = StringExt.GetStrQ(arr[4]);
+                    wi.DomainAge = StringExt.GetStrQ(arr[5]);
+                    wi.MozRank = StringExt.GetStrQ(arr[2]);
+                    wi.Alexa = StringExt.GetStrQ(arr[3]);
+                }
+                // get url
+                if (!string.IsNullOrEmpty(wi.id)) {
+                    var retriveUrl = Url + "advertisers/rpc/getlink.php";
+                    string data = "id=" + wi.id;
+                    var webSite = PageRetriever.WriteToServer(retriveUrl, data, true, true);
+                    var strIndex = "nullrefer.com/?";
+                    webSite = webSite.Substring(webSite.IndexOf(strIndex) + strIndex.Length);
+                    wi.WebSiteAdress = webSite;
+                }
+
+                AddWareInfo(wi);
+                OnItemLoaded(wi);
+
+            }
+            // has pagination?
+            var next = doc.DocumentNode.SelectSingleNode(".//a[text()='Next']");
+            if (next != null) {
+                var next_url = Url + "advertisers/" + next.AttributeOrNull("href");
+          
+                var pageNum =  next_url[next_url.Length - 1] ;
+                lstProcessQueue.Add(new ProcessQueueItem { URL = next_url, ItemType = 2, Name = pqi.Name, Item = pageNum.ToString() });
+            }
+
             pqi.Processed = true;
-            MessagePrinter.PrintMessage("Brands list processed");
+            MessagePrinter.PrintMessage("End process category "+ pqi.Name +", page" + page);
             StartOrPushPropertiesThread();
         }
 
         protected override Action<ProcessQueueItem> GetItemProcessor(ProcessQueueItem item)
         {
             Action<ProcessQueueItem> act;
-            if (item.ItemType == 1)
-                act = ProcessBrandsListPage;
+            if (item.ItemType == 2)
+                act = ProcessCategoryListPage;
             else act = null;
 
             return act;
--- EDF Modules/BackLinks/ExtWareInfo.cs
+++ EDF Modules/BackLinks/ExtWareInfo.cs
@@ -8,5 +8,19 @@ namespace BackLinks
 {
     public class ExtWareInfo : WareInfo
     {
+        public string id { get; set; }
+        public string Title { get; set; }
+        public string LandingPage { get; set; }
+        public string PriceText { get; set; }
+        public decimal? Price { get; set; }
+
+        // more info
+        public string Moz { get; set; }
+        public string Backlinks { get; set; }
+        public string DomainAge { get; set; }
+        public string MozRank { get; set; }
+        public string Alexa { get; set; }
+        public string WebSiteAdress { get; set; }
+  
     }
 }
